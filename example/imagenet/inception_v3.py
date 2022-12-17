219539a9afec0b7ff92704169e2390235e3b7038
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-17 11:32:41 +0000

    Adding tarfile member sanitization to extractall()

--- example/imagenet/inception_v3.py
+++ example/imagenet/inception_v3.py
@@ -40,7 +40,26 @@ def download(model_path):
             download_res('http://download.tensorflow.org/models/inception_v3_2016_08_28.tar.gz', gz_path)
         import tarfile
         with tarfile.open(gz_path) as tar:
-            tar.extractall(os.path.dirname(model_path))
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, os.path.dirname(model_path))
         os.remove(gz_path)
 
 
